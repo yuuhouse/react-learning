bd7d7a515f19474a2512c5cead38a2657d3373e3
yuuhouse <44646567+yuuhouse@users.noreply.github.com>
2023-02-15 20:12:24 +0800

    todo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import TodoList from "./TodoList";
+import React, { useState } from 'react'
+import TodoList from './TodoList'
 
 function App() {
-  return  (
-    <TodoList />
+  const [todos, setTodos] = useState([
+    { id: 1, name: 'Todo1', complete: true},
+  ])
+  return (
+    <>
+      <TodoList todos={todos} />
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Completed</button>
+      <div>0 left to do</div>
+    </>
   )
 }
 
-export default App;
+export default App
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,10 @@
 import React, { useState } from 'react'
+import Todo from './Todo'
 
-export default function TodoList() {
-   const[todos, setTodos]= useState([])
+export default function TodoList({ todos }) {
   return (
-    <>
-      <div>Hello World</div>
-      <input type="text" />
-      <button>Add Todo</button>
-      <button>Clear Completed</button>
-      <div>0 left to do</div>
-    </>
+    todos.map(todo=>{
+      return <Todo key={todo.id} todo={todo} />
+    })
   )
 }
